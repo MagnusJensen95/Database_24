a36192046bc504b45e7a043ca8f169aac6462b13
Mortenbaws <morten2094@hotmail.com>
2016-04-29 13:50:11 +0200

    added last JUnit test for vaerkfoerer all should now have junit testing

---
+++ src/junit/JUnit_Vaerkfoerer.java
@@ -0,0 +1,89 @@
+package junit;
+
+import static org.junit.Assert.*;
+
+import java.util.List;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.FixMethodOrder;
+import org.junit.Test;
+import org.junit.runners.MethodSorters;
+
+import daoimpl01917.MYSQLFarmaceutDAO;
+import daoimpl01917.MYSQLVaerkfoererDAO;
+import daointerfaces01917.DALException;
+import dto01917.FarmaceutDTO;
+import dto01917.VaerkfoererDTO;
+
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
+public class JUnit_Vaerkfoerer {
+
+	private static MYSQLVaerkfoererDAO vaerkfoerer;
+	private static VaerkfoererDTO vaerkfoererDTO;
+	
+	@Test
+	public void a_getListVaerkfoerer(){
+		vaerkfoerer = new MYSQLVaerkfoererDAO();
+		vaerkfoererDTO = new VaerkfoererDTO("Plebnus", "PN", "121291-1212", "suckit");
+		try{
+			List<VaerkfoererDTO> liste = vaerkfoerer.getVaerkfoererList();
+			if(liste == null){
+				fail("Gotten list is null!");
+			}
+			else{
+				if(liste.size() == 0){
+					fail("Either database is empty, or couldnt get proper list!");
+				}
+			}
+		} catch (DALException e) {
+			fail("Could not get list of vaerkfoerers!");
+		}
+	}
+	
+	@Test
+	public void b_addVaerkfoerer(){
+		try {
+			List<VaerkfoererDTO> liste = vaerkfoerer.getVaerkfoererList();
+			for(int i = 0; i < liste.size(); i++){
+				if(liste.get(i).getCpr().equals(vaerkfoererDTO.getCpr())){
+					fail("Already exist vaerkfoerer with those and cant add!");
+				}
+			}
+		} catch (DALException e1) {
+			fail("failed in getting list for add vaerkfoerer");
+		}
+		try {
+			vaerkfoerer.createVaerkfoerer(vaerkfoererDTO);
+		} catch (DALException e) {
+			fail("Could not create vaerkfoerer");
+		}
+	}
+
+	@Test
+	public void c_getVaerkfoerer() {
+		try {
+			VaerkfoererDTO test = vaerkfoerer.getVaerkfoerer(vaerkfoererDTO.getvaerkId());
+			if(test != null){
+				assertEquals(test.getCpr(), vaerkfoererDTO.getCpr());
+			}
+			else{
+				fail("Gotten vaerkfoerer is null!");
+			}
+		} catch (DALException e) {
+			fail("Could not get vaerkfoerer!");
+		}
+	}
+	
+	@Test
+	public void d_updateVaerkfoerer(){
+		try {
+			vaerkfoererDTO.setvaerkNavn("testnus");
+			vaerkfoerer.updateVaerkfoerer(vaerkfoererDTO, vaerkfoererDTO.getvaerkId());
+			VaerkfoererDTO temp = vaerkfoerer.getVaerkfoerer(vaerkfoererDTO.getvaerkId());
+			assertEquals(temp.getCpr(), vaerkfoererDTO.getCpr());
+		} catch (DALException e) {
+			fail("error getting updating vaerkfoerer!");
+		}
+	}
+}
